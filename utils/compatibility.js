f61adf0883e1f67b5b5161c0e0981647689c2cf6
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-06-17 13:56:02 +0200

    chore(compat): remove unused server compatibility.json
    The server only exposes its version via package.json; drop the unused metadata file, validate script, and pkg asset.

--- utils/compatibility.js
+++ utils/compatibility.js
@@ -1,26 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-const COMPATIBILITY_FILENAME = "compatibility.json";
-
-function readCompatibilityFile(baseDir) {
-  const candidates = [
-    path.join(baseDir, COMPATIBILITY_FILENAME),
-    path.join(__dirname, "..", COMPATIBILITY_FILENAME),
-  ];
-  for (const filePath of candidates) {
-    try {
-      if (fs.existsSync(filePath)) {
-        const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
-        return { data, filePath };
-      }
-    } catch (_) {
-      // try next
-    }
-  }
-  return { data: null, filePath: null };
-}
-
 function readPackageVersion(baseDir = path.join(__dirname, "..")) {
   const packageJsonPath = path.join(baseDir, "package.json");
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
@@ -49,8 +29,6 @@ function buildServerInfo(baseDir = path.join(__dirname, "..")) {
 }
 
 module.exports = {
-  COMPATIBILITY_FILENAME,
-  readCompatibilityFile,
   readPackageVersion,
   buildServerInfo,
 };
